099ea779fb64d51a2a0bed9053a293d6433484f2
K4maS <107948849+K4maS@users.noreply.github.com>
2023-07-16 11:10:55 +0500

    task-done

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-
-function openSearchBlock() {
-    const searchActivateBtn = document.querySelector('.weather-search-activate-btn');
-    const searchBlock = document.querySelector('.weather-search-block');
-    const searchBlockClose = document.querySelector('.weather-search-block-close-btn');
-
-    searchActivateBtn.addEventListener('click', () => {
-        searchBlock.classList.add('search-block--active');
-    });
-    searchBlockClose.addEventListener('click', () => {
-        searchBlock.classList.remove('search-block--active');
-    });
-}
-
-function changingWeatherGap() {
-    const weekSwitchBtn = document.querySelector('.btn-for-week');
-    const hourlySwitchBtn = document.querySelector('.btn-hourly');
-    const forWeekList = document.querySelector('.list-for-week');
-    const hourlyList = document.querySelector('.list-hourly');
-
-    weekSwitchBtn.addEventListener('click', () => {
-        forWeekList.classList.add('list-active');
-        hourlyList.classList.remove('list-active');
-        weekSwitchBtn.classList.add('item__gap-btn--active');
-        hourlySwitchBtn.classList.remove('item__gap-btn--active');
-        weekSwitchBtn.setAttribute('disabled', 'true');
-        hourlySwitchBtn.removeAttribute('disabled');
-    });
-    hourlySwitchBtn.addEventListener('click', () => {
-        forWeekList.classList.remove('list-active');
-        hourlyList.classList.add('list-active');
-        weekSwitchBtn.classList.remove('item__gap-btn--active');
-        hourlySwitchBtn.classList.add('item__gap-btn--active');
-        hourlySwitchBtn.setAttribute('disabled', 'true');
-        weekSwitchBtn.removeAttribute('disabled');
-    });
-}
-
 (() => {
+    function openSearchBlock() {
+        const searchActivateBtn = document.querySelector('.weather-search-activate-btn');
+        const searchBlock = document.querySelector('.weather-search-block');
+        const searchBlockClose = document.querySelector('.weather-search-block-close-btn');
+
+        searchActivateBtn.addEventListener('click', () => {
+            searchBlock.classList.add('search-block--active');
+        });
+        searchBlockClose.addEventListener('click', () => {
+            searchBlock.classList.remove('search-block--active');
+        });
+    }
+
+    function changingWeatherGap() {
+        const weekSwitchBtn = document.querySelector('.btn-for-week');
+        const hourlySwitchBtn = document.querySelector('.btn-hourly');
+        const forWeekList = document.querySelector('.list-for-week');
+        const hourlyList = document.querySelector('.list-hourly');
+
+        weekSwitchBtn.addEventListener('click', () => {
+            forWeekList.classList.add('list-active');
+            hourlyList.classList.remove('list-active');
+            weekSwitchBtn.classList.add('item__gap-btn--active');
+            hourlySwitchBtn.classList.remove('item__gap-btn--active');
+            weekSwitchBtn.setAttribute('disabled', 'true');
+            hourlySwitchBtn.removeAttribute('disabled');
+        });
+        hourlySwitchBtn.addEventListener('click', () => {
+            forWeekList.classList.remove('list-active');
+            hourlyList.classList.add('list-active');
+            weekSwitchBtn.classList.remove('item__gap-btn--active');
+            hourlySwitchBtn.classList.add('item__gap-btn--active');
+            hourlySwitchBtn.setAttribute('disabled', 'true');
+            weekSwitchBtn.removeAttribute('disabled');
+        });
+    }
+
+
     document.addEventListener('DOMContentLoaded', () => {
         openSearchBlock();
         changingWeatherGap();
