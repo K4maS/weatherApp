486961fd81b6ef99b64117a68b24202e381e3e3c
K4maS <107948849+K4maS@users.noreply.github.com>
2023-07-17 20:21:24 +0500

    изменил коммент

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { openSearchBlock } from "./searchBlock.js";
 import { changingWeatherGap } from "./weatherGap.js";
 import { changingTheme } from "./theme.js";
 
-// Чтобы все работало нужно запускать через openServer
+// Чтобы все работало нужно запускать через live Server
 
 (() => {
     document.addEventListener('DOMContentLoaded', () => {
