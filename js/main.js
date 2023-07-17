7d98505c1ac2c6ce72e7265cb267fcf570a56248
K4maS <107948849+K4maS@users.noreply.github.com>
2023-07-17 20:17:48 +0500

    task-done

--- js/main.js
+++ js/main.js
@@ -37,8 +37,36 @@
     }
 
 
+    function changingTheme() {
+        const themeBtn = document.querySelector('.theme-changing-btn');
+        themeBtn.addEventListener('click', function (e) {
+            if (e.srcElement.checked) {
+                document.documentElement.style.setProperty("--text", "rgba(230, 230, 230, 1)");
+                document.documentElement.style.setProperty("--theme-toggle", "rgba(236, 110, 77, 1)");
+                document.documentElement.style.setProperty("--btn-border", "rgba(172, 172, 172, 1)");
+                document.documentElement.style.setProperty("--right-bg-color", "rgba(16, 14, 28, 1)");
+                document.documentElement.style.setProperty("--aside-bg", "rgba(33, 35, 49, 1)");
+                document.documentElement.style.setProperty("--card-color", "rgba(33, 35, 49, 1)");
+                document.documentElement.style.setProperty("--shadow", "transporent");
+                document.documentElement.style.setProperty("--gray-text", "rgba(109, 109, 109, 1)");
+            }
+
+            else {
+                document.documentElement.style.setProperty("--text", " rgba(72, 72, 74, 1)");
+                document.documentElement.style.setProperty("--theme-toggle", " rgba(72, 72, 74, 1)");
+                document.documentElement.style.setProperty("--btn-border", " rgba(172, 172, 172, 1)");
+                document.documentElement.style.setProperty("--right-bg-color", " rgba(241, 241, 241, 1)");
+                document.documentElement.style.setProperty("--aside-bg", " #fff");
+                document.documentElement.style.setProperty("--card-color", " #fff");
+                document.documentElement.style.setProperty("--shadow", " rgba(0, 0, 0, 0.05)");
+                document.documentElement.style.setProperty("--gray-text", " rgba(211, 211, 211, 1)");
+            }
+        })
+    }
+
     document.addEventListener('DOMContentLoaded', () => {
         openSearchBlock();
         changingWeatherGap();
+        changingTheme();
     })
 })();
